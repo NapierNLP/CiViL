2932164e1dca0b25cb4eeb834901db1d8df42a56
yanchao-yu <magicyuyanchao@gmail.com>
2022-11-15 14:24:30 +0000

    update the asr and tts with fixing some bugs. please remmember to install speech_recognition, pyaudio and pyttsx3

--- core/src/speech/asr.py
+++ core/src/speech/asr.py
@@ -3,188 +3,38 @@ from __future__ import division
 import os
 import re
 import sys
-
-from google.cloud import speech
-
+import speech_recognition as sr
 import pyaudio
-from six import moves
 
-# Audio recording parameters
-# RATE = 16000
-# CHUNK = int(RATE / 10)  # 100ms
+# Initialize the recognizer
+r = sr.Recognizer()
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 48000
-RECORD_SECONDS = 2
-WAVE_OUTPUT_FILENAME = "output.wav"
-INPUT_DEVICE_INDEX = 3
+while (1):
 
-# setting Google credential
-os.environ[
-    'GOOGLE_APPLICATION_CREDENTIALS'] = '/home/y-yu/WorkSpace/codebase/CiViL/core/turnkey-mender-362510-1af6017680ea.json'
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
 
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
 
-class MicrophoneStream(object):
-    """Opens a recording stream as a generator yielding the audio chunks."""
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level
+            r.adjust_for_ambient_noise(source2, duration=0.2)
 
-    def __init__(self, rate, chunk):
-        self._rate = rate
-        self._chunk = chunk
+            # listens for the user's input
+            audio2 = r.listen(source2)
 
-        # Create a thread-safe buffer of audio data
-        self._buff = moves.queue.Queue()
-        self.closed = True
+            # Using google to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
 
-    def __enter__(self):
-        self._audio_interface = pyaudio.PyAudio()
+            print("Did you say ", MyText)
+            SpeakText(MyText)
 
-        # check the device information
-        info = self._audio_interface.get_host_api_info_by_index(0)
-        print('info: {}'.format(info))
-        numdevices = info.get('deviceCount')
-        print('number of devices: {}'.format(numdevices))
-        for i in range(0, numdevices):
-            if (self._audio_interface.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-                print("Input Device id ", i, " - ", self._audio_interface.get_device_info_by_host_api_device_index(0, i).get('name'))
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
 
-        self._audio_stream = self._audio_interface.open(
-            format=pyaudio.paInt16,
-            # The API currently only supports 1-channel (mono) audio
-            # https://goo.gl/z757pE
-            channels=CHANNELS,
-            rate=self._rate,
-            input=True,
-            frames_per_buffer=self._chunk,
-            stream_callback=self._fill_buffer,
-        )
-
-        self.closed = False
-
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self._audio_stream.stop_stream()
-        self._audio_stream.close()
-        self.closed = True
-        # Signal the generator to terminate so that the client's
-        # streaming_recognize method will not block the process termination.
-        self._buff.put(None)
-        self._audio_interface.terminate()
-
-    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
-        """Continuously collect data from the audio stream, into the buffer."""
-        self._buff.put(in_data)
-        return None, pyaudio.paContinue
-
-    def generator(self):
-        while not self.closed:
-            # Use a blocking get() to ensure there's at least one chunk of
-            # data, and stop iteration if the chunk is None, indicating the
-            # end of the audio stream.
-            chunk = self._buff.get()
-            if chunk is None:
-                return
-            data = [chunk]
-
-            # Now consume whatever other data's still buffered.
-            while True:
-                try:
-                    chunk = self._buff.get(block=False)
-                    if chunk is None:
-                        return
-                    data.append(chunk)
-                except moves.queue.Empty:
-                    break
-
-            yield b"".join(data)
-
-
-def listen_print_loop(responses):
-    """Iterates through server responses and prints them.
-
-    The responses passed is a generator that will block until a response
-    is provided by the server.
-
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
-
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
-    """
-    num_chars_printed = 0
-    for response in responses:
-        if not response.results:
-            continue
-
-        # The `results` list is consecutive. For streaming, we only care about
-        # the first result being considered, since once it's `is_final`, it
-        # moves on to considering the next utterance.
-        result = response.results[0]
-        if not result.alternatives:
-            continue
-
-        # Display the transcription of the top alternative.
-        transcript = result.alternatives[0].transcript
-
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-        #
-        # If the previous result was longer than this one, we need to print
-        # some extra spaces to overwrite the previous result
-        overwrite_chars = " " * (num_chars_printed - len(transcript))
-
-        if not result.is_final:
-            sys.stdout.write(transcript + overwrite_chars + "\r")
-            sys.stdout.flush()
-
-            num_chars_printed = len(transcript)
-
-        else:
-            print(transcript + overwrite_chars)
-
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
-            if re.search(r"\b(exit|quit)\b", transcript, re.I):
-                print("Exiting..")
-                break
-
-            num_chars_printed = 0
-
-
-def main():
-    # See http://g.co/cloud/speech/docs/languages
-    # for a list of supported languages.
-    language_code = "en-US"  # a BCP-47 language tag
-
-
-    # client = speech.SpeechClient()
-    # config = speech.RecognitionConfig(
-    #     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-    #     sample_rate_hertz=RATE,
-    #     language_code=language_code,
-    # )
-    #
-    # streaming_config = speech.StreamingRecognitionConfig(
-    #     config=config, interim_results=True
-    # )
-    #
-    # with MicrophoneStream(RATE, CHUNK) as stream:
-    #     audio_generator = stream.generator()
-    #     requests = (
-    #         speech.StreamingRecognizeRequest(audio_content=content)
-    #         for content in audio_generator
-    #     )
-    #
-    #     responses = client.streaming_recognize(streaming_config, requests)
-    #
-    #     # Now, put the transcription responses to use.
-    #     listen_print_loop(responses)
-
-
-if __name__ == "__main__":
-    main()
+    except sr.UnknownValueError:
+        print("unknown error occurred")
--- core/src/speech/tts.py
+++ core/src/speech/tts.py
@@ -1,40 +1,44 @@
 import os
+import pyaudio
+import pyttsx3
+import wave
+import sys
 
-from google.cloud import texttospeech
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+
+# from google.cloud import texttospeech
 
 # setting Google credential
-os.environ[
-    'GOOGLE_APPLICATION_CREDENTIALS'] = '/home/y-yu/WorkSpace/codebase/CiViL/core/turnkey-mender-362510-1af6017680ea.json'
+# os.environ[
+#     'GOOGLE_APPLICATION_CREDENTIALS'] = "C:/Users/carls/Downloads/turnkey-mender-362510-406e6c97578b.json"
 
 
 def synthesize_text(text):
     """Synthesizes speech from the input string of text."""
 
-    client = texttospeech.TextToSpeechClient()
-
-    input_text = texttospeech.SynthesisInput(text=text)
-
-    # Note: the voice can also be specified by name.
-    # Names of voices can be retrieved with client.list_voices().
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="en-US",
-        name="en-US-Standard-C",
-        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
-    )
-
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3
-    )
-
-    response = client.synthesize_speech(
-        request={"input": input_text, "voice": voice, "audio_config": audio_config}
-    )
-
-    # The response's audio_content is binary.
-    with open("../output.mp3", "wb") as out:
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
-
+    # client = texttospeech.TextToSpeechClient()
+    #
+    # input_text = texttospeech.SynthesisInput(text=text)
+    #
+    #
+    # # Note: the voice can also be specified by name.
+    # # Names of voices can be retrieved with client.list_voices().
+    # voice = texttospeech.VoiceSelectionParams(
+    #     language_code="en-US",
+    #     name="en-US-Standard-C",
+    #     ssml_gender=texttospeech.SsmlVoiceGender.MALE,
+    # )
+    #
+    # audio_config = texttospeech.AudioConfig(
+    #     audio_encoding=texttospeech.AudioEncoding.MP3
+    # )
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)  # Speed percent (can go over 100)
+    engine.setProperty('volume', 0.9)  # Volume 0-1
+    engine.say(text)  # play the speech
+    engine.runAndWait()
 
 if __name__ == "__main__":
-    synthesize_text("I'm fine, thanks.")
+    synthesize_text("You then have to place each of the slices onto a flattened out paper cupcake case on a baking tray, or into a greased muffin tin..")
