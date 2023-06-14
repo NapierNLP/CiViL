35338707fc484028c7454c9688a9cf5974e3f8ba
yanchao-yu <magicyuyanchao@gmail.com>
2023-06-14 07:41:19 +0100

    add yolo file and shell script

--- ultralytics-main/ultralytics/train.py
+++ ultralytics-main/ultralytics/train.py
@@ -5,8 +5,8 @@ model = YOLO('yolov8n.pt')
 
 # Training.
 results = model.train(
-    data='datasets/ingredients/data.yaml',
-    # data='datasets/utensils/data.yaml',
+    data='/home/y-yu/WorkSpace/codebase/CiViL/ultralytics-main/ultralytics/datasets/ingredients/data.yaml',
+    # data='/home/y-yu/WorkSpace/codebase/CiViL/ultralytics-main/ultralytics/datasets/utensils/data.yaml',
     imgsz=1280,
     epochs=50,
     batch=8,
